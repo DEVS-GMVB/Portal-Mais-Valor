26c66a76c7bd637bbe48673182054198dde99da2
Saulo Mattos <saulomm15@gmail.com>
2021-01-14 17:30:14 -0300

    Mascaras Ok

---
+++ assets/js/mascaras.js
@@ -0,0 +1,121 @@
+function mascaraMutuario(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout('execmascara()',1)
+}
+ 
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+ 
+function cpfCnpj(v){
+ 
+    //Remove tudo o que não é dígito
+    v=v.replace(/\D/g,"")
+ 
+    if (v.length <= 14) { //CPF
+ 
+        //Coloca um ponto entre o terceiro e o quarto dígitos
+        v=v.replace(/(\d{3})(\d)/,"$1.$2")
+ 
+        //Coloca um ponto entre o terceiro e o quarto dígitos
+        //de novo (para o segundo bloco de números)
+        v=v.replace(/(\d{3})(\d)/,"$1.$2")
+ 
+        //Coloca um hífen entre o terceiro e o quarto dígitos
+        v=v.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+ 
+    } else { //CNPJ
+ 
+        //Coloca ponto entre o segundo e o terceiro dígitos
+        v=v.replace(/^(\d{2})(\d)/,"$1.$2")
+ 
+        //Coloca ponto entre o quinto e o sexto dígitos
+        v=v.replace(/^(\d{2})\.(\d{3})(\d)/,"$1.$2.$3")
+ 
+        //Coloca uma barra entre o oitavo e o nono dígitos
+        v=v.replace(/\.(\d{3})(\d)/,".$1/$2")
+ 
+        //Coloca um hífen depois do bloco de quatro dígitos
+        v=v.replace(/(\d{4})(\d)/,"$1-$2")
+ 
+    }
+ 
+    return v
+ 
+}
+
+
+function fMasc(objeto,mascara) {
+    obj=objeto
+    masc=mascara
+    setTimeout("fMascEx()",1)
+}
+
+function fMascEx() {
+    obj.value=masc(obj.value)
+}
+
+function mData(data){
+    data=data.replace(/\D/g,"")
+    data=data.replace(/(\d{2})(\d)/,"$1/$2")
+    data=data.replace(/(\d{2})(\d)/,"$1/$2")
+    return data
+}
+
+function mTelefone(telefone){
+    telefone=telefone.replace(/\D/g,"")
+    telefone=telefone.replace(/(\d{0})(\d)/,"$1($2")
+    telefone=telefone.replace(/(\d{2})(\d)/,"$1)$2")
+    telefone=telefone.replace(/(\d{4})(\d)/,"$1-$2")
+    return telefone
+}
+
+function moeda(a, e, r, t) {
+    let n = ""
+      , h = j = 0
+      , u = tamanho2 = 0
+      , l = ajd2 = ""
+      , o = window.Event ? t.which : t.keyCode;
+      a.value = a.value.replace('R$ ','');            
+    if (n = String.fromCharCode(o),
+    -1 == "0123456789".indexOf(n))
+        return !1;
+    for (u = a.value.replace('R$ ','').length,
+    h = 0; h < u && ("0" == a.value.charAt(h) || a.value.charAt(h) == r); h++)
+        ;
+    for (l = ""; h < u; h++)
+        -1 != "0123456789".indexOf(a.value.charAt(h)) && (l += a.value.charAt(h));
+    if (l += n,
+    0 == (u = l.length) && (a.value = ""),
+    1 == u && (a.value = "R$ 0" + r + "0" + l),
+    2 == u && (a.value = "R$ 0" + r + l),
+    u > 2) {
+        for (ajd2 = "",
+        j = 0,
+        h = u - 3; h >= 0; h--)
+            3 == j && (ajd2 += e,
+            j = 0),
+            ajd2 += l.charAt(h),
+            j++;
+        for (a.value = "R$ ",
+        tamanho2 = ajd2.length,
+        h = tamanho2 - 1; h >= 0; h--)
+            a.value += ajd2.charAt(h);
+        a.value += r + l.substr(u - 2, u)
+    }
+    return !1
+}
+
+function validar(){
+
+    if(cpf ==! null){
+        
+        alert(cpf.value)
+
+    }else{
+        
+        alert('cpf esta vazio')
+    }
+
+}
