0fa00aa334825f074ac444137d554cd63d7ba9ec
SauloMattos <40602464+SauloMattos@users.noreply.github.com>
2021-01-16 11:19:34 -0300

    ALTERAÇÕES NAS MÁSCARAS

--- assets/js/mascaras.js
+++ assets/js/mascaras.js
@@ -3,7 +3,74 @@ function mascaraMutuario(o,f){
     v_fun=f
     setTimeout('execmascara()',1)
 }
- 
+
+function fMasc(objeto,mascara) {
+    obj=objeto
+    masc=mascara
+    setTimeout("fMascEx()",1)
+}
+
+function fMascEx() {
+    obj.value=masc(obj.value)
+}
+
+function mData(data){
+    data=data.replace(/\D/g,"")
+    data=data.replace(/(\d{2})(\d)/,"$1/$2")
+    data=data.replace(/(\d{2})(\d)/,"$1/$2")
+    return data
+}
+
+function mTelefone(telefone){
+    telefone=telefone.replace(/\D/g,"")
+    telefone=telefone.replace(/(\d{0})(\d)/,"$1($2")
+    telefone=telefone.replace(/(\d{2})(\d)/,"$1)$2")
+    telefone=telefone.replace(/(\d{4})(\d)/,"$1-$2")
+    return telefone
+}
+
+function mCEP(cep){
+    cep=cep.replace(/\D/g,"")
+    cep=cep.replace(/(\d{5})(\d)/,"$1-$2")
+    return cep
+}
+
+function moeda(a, e, r, t) {
+    let n = ""
+      , h = j = 0
+      , u = tamanho2 = 0
+      , l = ajd2 = ""
+      , o = window.Event ? t.which : t.keyCode;
+      a.value = a.value.replace('R$ ','');            
+    if (n = String.fromCharCode(o),
+    -1 == "0123456789".indexOf(n))
+        return !1;
+    for (u = a.value.replace('R$ ','').length,
+    h = 0; h < u && ("0" == a.value.charAt(h) || a.value.charAt(h) == r); h++)
+        ;
+    for (l = ""; h < u; h++)
+        -1 != "0123456789".indexOf(a.value.charAt(h)) && (l += a.value.charAt(h));
+    if (l += n,
+    0 == (u = l.length) && (a.value = ""),
+    1 == u && (a.value = "R$ 0" + r + "0" + l),
+    2 == u && (a.value = "R$ 0" + r + l),
+    u > 2) {
+        for (ajd2 = "",
+        j = 0,
+        h = u - 3; h >= 0; h--)
+            3 == j && (ajd2 += e,
+            j = 0),
+            ajd2 += l.charAt(h),
+            j++;
+        for (a.value = "R$ ",
+        tamanho2 = ajd2.length,
+        h = tamanho2 - 1; h >= 0; h--)
+            a.value += ajd2.charAt(h);
+        a.value += r + l.substr(u - 2, u)
+    }
+    return !1
+}
+
 function execmascara(){
     v_obj.value=v_fun(v_obj.value)
 }
@@ -45,77 +112,24 @@ function cpfCnpj(v){
  
 }
 
-
-function fMasc(objeto,mascara) {
-    obj=objeto
-    masc=mascara
-    setTimeout("fMascEx()",1)
-}
-
-function fMascEx() {
-    obj.value=masc(obj.value)
-}
-
-function mData(data){
-    data=data.replace(/\D/g,"")
-    data=data.replace(/(\d{2})(\d)/,"$1/$2")
-    data=data.replace(/(\d{2})(\d)/,"$1/$2")
-    return data
-}
-
-function mTelefone(telefone){
-    telefone=telefone.replace(/\D/g,"")
-    telefone=telefone.replace(/(\d{0})(\d)/,"$1($2")
-    telefone=telefone.replace(/(\d{2})(\d)/,"$1)$2")
-    telefone=telefone.replace(/(\d{4})(\d)/,"$1-$2")
-    return telefone
-}
-
-function moeda(a, e, r, t) {
-    let n = ""
-      , h = j = 0
-      , u = tamanho2 = 0
-      , l = ajd2 = ""
-      , o = window.Event ? t.which : t.keyCode;
-      a.value = a.value.replace('R$ ','');            
-    if (n = String.fromCharCode(o),
-    -1 == "0123456789".indexOf(n))
-        return !1;
-    for (u = a.value.replace('R$ ','').length,
-    h = 0; h < u && ("0" == a.value.charAt(h) || a.value.charAt(h) == r); h++)
-        ;
-    for (l = ""; h < u; h++)
-        -1 != "0123456789".indexOf(a.value.charAt(h)) && (l += a.value.charAt(h));
-    if (l += n,
-    0 == (u = l.length) && (a.value = ""),
-    1 == u && (a.value = "R$ 0" + r + "0" + l),
-    2 == u && (a.value = "R$ 0" + r + l),
-    u > 2) {
-        for (ajd2 = "",
-        j = 0,
-        h = u - 3; h >= 0; h--)
-            3 == j && (ajd2 += e,
-            j = 0),
-            ajd2 += l.charAt(h),
-            j++;
-        for (a.value = "R$ ",
-        tamanho2 = ajd2.length,
-        h = tamanho2 - 1; h >= 0; h--)
-            a.value += ajd2.charAt(h);
-        a.value += r + l.substr(u - 2, u)
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+  
+  function validate() {
+    const $result = $("#result");
+    const email = $("#email").val();
+    $result.text("");
+  
+    if (validateEmail(email)) {
+      $result.text(email + " is valid :)");
+      $result.css("color", "green");
+    } else {
+      $result.text(email + " is not valid :(");
+      $result.css("color", "red");
     }
-    return !1
-}
-
-function validar(){
-
-    if(cpf ==! null){
-        
-        alert(cpf.value)
-
-    }else{
-        
-        alert('cpf esta vazio')
-    }
-
-}
+    return false;
+  }
+  
+  $("#validate").on("click", validate);
