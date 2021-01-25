5ecabba9affa6a4779a51c9d7d81379f2038c1e1
marvim1990 <marcosvinim.cruz@gmail.com>
2021-01-25 11:23:22 -0300

    25012021

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -1,9 +1,10 @@
 // VARS
-var gerente = document.getElementById('exampleFormControlSelectGerente');
-var filial = document.getElementById('exampleFormControlFilial');
-var supervisor = document.getElementById('exampleFormControlSupervisor');
-var mes = document.getElementById('exampleFormControlMes');
-var mesDemissao = document.getElementById('exampleFormControlMesDemissao');
+let gerente = document.getElementById('exampleFormControlSelectGerente');
+let filial = document.getElementById('exampleFormControlFilial');
+let supervisor = document.getElementById('exampleFormControlSupervisor');
+let mes = document.getElementById('exampleFormControlMes');
+let mesDemissao = document.getElementById('exampleFormControlMesDemissao');
+window.onload = function () {
 
 var requestOptions = {
     method: 'GET',
@@ -14,10 +15,8 @@ var requestOptions = {
   fetch("http://172.16.0.197:3000/user/gerente", requestOptions)
     .then(response => response.json().then(function(data){
         for(let i = 0; i < data.length; i++){
-            gerente.innerHTML += '<option value="'+i+'">'+data[i].gerente+'</option>;'
+            gerente.innerHTML += '<option value="'+data[i].gerente+'">'+data[i].gerente+'</option>;'
         }
-
-
     })).catch(error => console.log('error', error));
 
     
@@ -25,7 +24,7 @@ var requestOptions = {
     .then(response => response.json()
     .then(function(data){
         for(let i = 0; i < data.length; i++){
-            filial.innerHTML += '<option value="'+i+'">'+data[i].filial+'</option>;'
+            filial.innerHTML += '<option value="'+data[i].filial+'">'+data[i].filial+'</option>;'
         }
     }))
     .catch(error => console.log('error', error));
@@ -34,23 +33,70 @@ var requestOptions = {
     .then(response => response.json()
     .then(function(data){
         for(let i = 0; i < data.length; i++){
-            supervisor.innerHTML += '<option value="'+i+'">'+data[i].parceiro+'</option>;'
+            supervisor.innerHTML += '<option value="'+data[i].parceiro+'">'+data[i].parceiro+'</option>;'
         }
     }))
     .catch(error => console.log('error', error));
-
-
-
-
-
-
-
-// Mês de admissão
-for(let i = 1; i <= 12; i++) {
-    mes.innerHTML += '<option value="'+i+'">'+i+'</option>;'
 }
+const colocar = document.getElementById('incluir');
 
-for(let i = 1; i <= 12; i++) {
-    mesDemissao.innerHTML += '<option value="'+i+'">'+i+'</option>;'
 
-}
+colocar.addEventListener('click', () => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+  
+    let parceiro = document.getElementById('validationCustomFuncionario').value;
+    let cnpj = document.getElementById('cpfcnpj').value;
+    let supervisor = document.getElementById('exampleFormControlSupervisor').value;
+    let status = document.getElementById('status').value;
+    let gerente = document.getElementById('exampleFormControlSelectGerente').value;
+    let filial = document.getElementById('exampleFormControlFilial').value;
+    let mes_admissao = document.getElementById('exampleFormControlMes').value;
+    let mes_demissao = document.getElementById('exampleFormControlMesDemissao').value;
+    let tipo = document.getElementById('exampleFormControlTipo').value;
+    let superintendente = document.getElementById('exampleFormControlSelect1Superintendente').value;
+    // console.log(cnpj,status,supervisor,gerente,filial, mes_admissao, mes_demissao, tipo,superintendente);
+    var raw = JSON.stringify({
+        parceiro:parceiro,
+        cnpj:cnpj,
+        supervisor:supervisor,
+        status:status,
+        gerente:gerente,
+        filial:filial,
+        mes_admissao:mes_admissao,
+        mes_demissao:mes_demissao,
+        tipo:tipo
+    })
+      
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+
+    // console.log(raw);
+    fetch("http://localhost:3000/user/search", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+        
+        for (const value of result) {
+            console.log(value);
+            var x = document.createElement("tr");
+            x.setAttribute("id", "myTr");
+            document.getElementById("table").appendChild(x);
+
+           
+          }
+
+    })
+    .catch(error => console.log('error', error));
+})
+
+
+ // 
+        
+        // row.insertCell(1).innerHTML = func.value;
+        // row.insertCell(2).innerHTML = cpfcnpj.value;
+        // row.insertCell(3).in
+        // document.getElementById('id').appendChild(row);
--- assets/js/cadastropesquisar.js
+++ assets/js/cadastropesquisar.js
@@ -1,48 +1,48 @@
-var colocar = document.getElementById('incluir');
-var func = document.getElementById('validationCustomFuncionario')
-var cpfcnpj = document.getElementById('cpfcnpj');
-var supervisor = document.getElementById('exampleFormControlSupervisor');
-var sel = document.getElementById('status');
-var supe = document.getElementById('exampleFormControlSupervisor');
-var gere = document.getElementById('exampleFormControlSelectGerente');
-var filal = document.getElementById('exampleFormControlFilial');
-var mesAd = document.getElementById('exampleFormControlMes');
-var mesDe = document.getElementById('exampleFormControlMesDemissao');
-var tipo = document.getElementById('exampleFormControlTipo');
-// Vars
 
+// Vars
+const colocar = document.getElementById('incluir');
 
 colocar.addEventListener('click', () => {
+  
+    let parceiro = document.getElementById('validationCustomFuncionario').value;
+    let cnpj = document.getElementById('cpfcnpj').value;
+    let supervisor = document.getElementById('exampleFormControlSupervisor').value;
+    let status = document.getElementById('status').value;
+    let gerente = document.getElementById('exampleFormControlSelectGerente').value;
+    let filial = document.getElementById('exampleFormControlFilial').value;
+    let mes_admissao = document.getElementById('exampleFormControlMes').value;
+    let mes_demissao = document.getElementById('exampleFormControlMesDemissao').value;
+    let tipo = document.getElementById('exampleFormControlTipo').value;
 
     var raw = JSON.stringify(
-        {"parceiro": func.value},
-        {"cnpj": cpfcnpj.value},
-        {"status": sel.value},
-        {"supervisor": supe.value},
-        {"gerente": gere.value},
-        {"filial": filal.value},
-        {"mes_admissao": mesAd.value},
-        {"mes_demissao" : mesDe.value},
-        {"tipo": tipo.value}
+        parceiro,
+        cnpj,
+        supervisor,
+        status,
+        gerente,
+        filial,
+        mes_admissao,
+        mes_demissao,
+        tipo
     )
       
-    var requestOptions = {
-        method: 'POST',
-        body: raw,
-        redirect: 'follow'
-    };
+    // var requestOptions = {
+    //     method: 'POST',
+    //     body: raw,
+    //     redirect: 'follow'
+    // };
 
-
-    fetch("http://172.16.0.197:3000/user/search", requestOptions)
-    .then(response => response.json()
-    .then(function(data){
-        var row = document.createElement('tr');
-        row.insertCell(0).innerHTML = filal.options[filal.selectedIndex].text;
-        row.insertCell(1).innerHTML = func.value;
-        row.insertCell(2).innerHTML = cpfcnpj.value;
-        row.insertCell(3).in
-        document.getElementById('id').appendChild(row);
-    }))
-    .catch(error => console.log('error', error));
+    console.log(raw)
+//     fetch("http://172.16.0.197:3000/user/search", requestOptions)
+//     .then(response => response.json()
+//     .then(function(data){
+//         var row = document.createElement('tr');
+//         row.insertCell(0).innerHTML = filal.options[filal.selectedIndex].text;
+//         row.insertCell(1).innerHTML = func.value;
+//         row.insertCell(2).innerHTML = cpfcnpj.value;
+//         row.insertCell(3).in
+//         document.getElementById('id').appendChild(row);
+//     }))
+//     .catch(error => console.log('error', error));
 })
 
