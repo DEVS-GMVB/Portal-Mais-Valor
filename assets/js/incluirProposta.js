cb68327d391169290d1fb30717fbd5ce0f5ea31f
marvim1990 <marcosvinim.cruz@gmail.com>
2021-02-17 16:30:37 -0300

    propostas

--- assets/js/incluirProposta.js
+++ assets/js/incluirProposta.js
@@ -1,20 +1,19 @@
-
 //variavel de ambiente
 const URL = `http://localhost:3000`;
 
 //variaveis de sessao
-const parceiro = sessionStorage.getItem('nome','nome');
-const tipo_parceiro = sessionStorage.getItem('tipo_parceiro','tipo_parceiro');
-const id_acesso = sessionStorage.getItem('id_acesso','id_acesso');
-  //**falta incluir na session */
-  // const supervisor = sessionStorage.getItem() 
-  // const gerente = sessionStorage.getItem()
+const parceiro = sessionStorage.getItem('nome', 'nome');
+const tipo_parceiro = sessionStorage.getItem('tipo_parceiro', 'tipo_parceiro');
+const id_acesso = sessionStorage.getItem('id_acesso', 'id_acesso');
+//**falta incluir na session */
+// const supervisor = sessionStorage.getItem() 
+// const gerente = sessionStorage.getItem()
 
 //inputs modal Nova proposta
 const numeroProposta = document.getElementById('numero-proposta-incluir');
 const dataCadastroIncluir = document.getElementById('data-cadastro-incluir');
 const banco = document.getElementById('banco-incluir');
-const status = document.getElementById('status-incluir');
+const status = document.getElementById('status-inclir');
 const produto = document.getElementById('produto-incluir');
 const tipoOperacao = document.getElementById('tipo-operacao-incluir');
 const valorEntregue = document.getElementById('valor-entregue-incluir');
@@ -48,7 +47,7 @@ const botaoIncluir = document.getElementById('incluir-button');
 //gets para popular options 
 
 window.onload = function () {
-  
+
   var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
@@ -57,16 +56,16 @@ window.onload = function () {
     redirect: 'follow'
   }
 
-  fetch(URL+"/user/proposta/produto", requestOptions)
+  fetch(URL + "/user/proposta/produto", requestOptions)
     .then(response => response.json())
     .then(function (data) {
-    data.forEach(element => {
-      produto.innerHTML += `<option value =${element.produto}>${element.produto}</option>`;
-    });
+      data.forEach(element => {
+        produto.innerHTML += `<option value =${element.produto}>${element.produto}</option>`;
+      });
     })
 
 
-  fetch(URL+"/user/proposta/tipo", requestOptions)
+  fetch(URL + "/user/proposta/tipo", requestOptions)
     .then(response => response.json())
     .then(function (data) {
       data.forEach(element => {
@@ -74,9 +73,9 @@ window.onload = function () {
       });
     })
 
-  fetch(URL+"/user/proposta/bancos", requestOptions)
+  fetch(URL + "/user/proposta/bancos", requestOptions)
     .then(response => response.json())
-        .then(function (data) {
+    .then(function (data) {
       data.forEach(element => {
         bancoPortador.innerHTML += `<option value =${element.banco}>${element.banco}</option>`;
       });
@@ -90,60 +89,78 @@ botaoIncluir.addEventListener('click', () => {
 
   const body = {
     // parceiro, id_acesso, supervisor, gerente, tipo_parceiro, 
-      proposta : numeroProposta.value,
-      data_envio:dataCadastroIncluir.value,
-      banco:banco.value,
-      status:status.value,
-      produto:produto.value,
-      tipo:tipoOperacao.value,
-      entregue:valorEntregue.value,
-      valor_troco:valorTroco.value,
-      convenio:convenio.value,
-      banco_port1:bancoPortador.value,
-      numero_portabilidade:portabilidade.value,
-      parcela:valorParcela.value,
-      seguro:seguro.value,
-      qtdp_pagaport1:parcelasPagas.value,
-      nome:nomeCliente.value,
-      cpf:cpfCliente.value,
-      telefone_ddd_1:ddd.value,
-      telefone1:telefoneCliente.value,
-      correntista:correntista.value,
-      telefone4:telefoneSmsCliente.value,
-      matricula:matricula.value,
-      agendamento:desejaAgendarHorario.value,
-      dia:melhorDatata.value,
-      horario:melhorHorario.value,
-      exercito:exercitoTemporario.value,
-      senha_exercito:codigoExercito.value,
-      sexo:sexo.value,
-      data_nascimento:dataNascimento.value,
-      email_cliente:email.value,
-      uf:uf.value,
-      observacao:observacao.value,
+    proposta: numeroProposta.value,
+    data_envio: dataCadastroIncluir.value,
+    banco: banco.value,
+    // status:status.value,
+    produto: produto.value,
+    tipo: tipoOperacao.value,
+    entregue: valorEntregue.value,
+    valor_troco: valorTroco.value,
+    convenio: convenio.value,
+    banco_port1: bancoPortador.value,
+    numero_portabilidade: portabilidade.value,
+    parcela: valorParcela.value,
+    seguro: seguro.value,
+    qtdp_pagaport1: parcelasPagas.value,
+    nome: nomeCliente.value,
+    cpf: cpfCliente.value,
+    telefone_ddd_1: ddd.value,
+    telefone1: telefoneCliente.value,
+    correntista: correntista.value,
+    telefone4: telefoneSmsCliente.value,
+    matricula: matricula.value,
+    agendamento: desejaAgendarHorario.value,
+    dia: melhorDatata.value,
+    horario: melhorHorario.value,
+    exercito: exercitoTemporario.value,
+    senha_exercito: codigoExercito.value,
+    sexo: sexo.value,
+    data_nascimento: dataNascimento.value,
+    email_cliente: email.value,
+    uf: uf.value,
+    observacao: observacao.value,
 
   }
 
   const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Content-Type", "application/json");
 
-    const raw = JSON.stringify(body);
+  const raw = JSON.stringify(body);
 
-    const requestOptions = {
+  const requestOptions = {
     method: 'POST',
     headers: myHeaders,
     body: raw,
     redirect: 'follow'
-    };
+  };
 
-  fetch(URL+"/user/proposta/inclusao", requestOptions)
-  .then(response => response.json())
-  .then(function (data) {
-   console.log(data)
-  //  data.codigo 
-  //enviar requisição de arquivos aqui
-  });
-}).catch(error => console.log('error' , error))
+  fetch(URL + "/user/proposta/inclusao", requestOptions)
+    .then(response => response.json())
+    .then(function (data) {
+      var codigo = data.codigo;
+
+      var input = document.querySelectorAll('form#files input[type="file"]')
+
+      var data = new FormData()
+      input.forEach(file => {
+        data.append(file.name, file.files[0])
+
+      });
 
 
+      fetch(URL + `/user/proposta/inclusao/arquivos?codigo=${codigo}`, {
+          method: 'POST',
+          body: data
+        })
+        .then((response) => response.json())
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
 
+    }).catch(error => console.log('error' , error))
+
+})
