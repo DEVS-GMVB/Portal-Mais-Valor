4caacd717ee13b424dea2ec42055776490bbef12
marvim1990 <marcosvinim.cruz@gmail.com>
2021-02-24 14:14:26 -0300

    pesquisa comissao ok

--- assets/js/incluirProposta.js
+++ assets/js/incluirProposta.js
@@ -227,5 +227,19 @@ botaoIncluir.addEventListener('click', () => {
 
     }).catch(error => console.log('error' , error))
 })
+function readURL(input) {
+  if (input.files && input.files[0]) {
+      var reader = new FileReader();
 
+      reader.onload = function (e) {
+          $('#proposta-file').attr('src', e.target.result);
+      }
+
+      reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$("#imgInp").change(function(){
+  readURL(this);
+});
 //////modal filtro
