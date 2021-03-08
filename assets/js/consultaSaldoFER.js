ee3c01697a0652dde903fdbe3d51b64601a32f7f
Cauesilvaa <43147371+Cauesilvaa@users.noreply.github.com>
2021-03-08 16:50:31 -0300

    consulta fer quase finalizado

--- assets/js/consultaSaldoFER.js
+++ assets/js/consultaSaldoFER.js
@@ -1,48 +1,275 @@
-const URL = 'http://localhost:3000'
+const URL = `http://localhost:3000`;
+
+// VARS OF BUTTONS
+const changeButtonInsert = document.getElementById("btn-incluir");
+const changeButtonUpdate = document.getElementById("icon-alterar");
+const makeInsert = document.getElementById("btn-insert");
+const buttonSearch = document.getElementById("btn-buscar");
+
+//OBJS
+const obj = {}
+const arrays = {
+    arrayChangeCods: [],
+    arrayRows: [],
+    arrayChangeRows: [],
+    contadorChangeCods: 0,
+    contadorChangeRows: 0
+}
 
 const breakModal = {
-    buttonInsert: function(){
-        document.getElementById('btn-incluir').innerHTML = `
-        <button type="button" class="btn btn-primary btn-icon-label" id="btn-incluir" onclick="insertFER()">
-                            <span class="btn-inner--icon">
-                                <i class="fas fa-plus"></i>
-                            </span>
-                            <span class="btn-inner--text">Incluir saldo FER</span>
-                        </button>
+    changeInsert: function () {
+        document.getElementById("changeButtons").innerHTML = `
+        <button type="button" class="btn btn-primary btn-icon-label" id="btn-insert" onclick="insert()">
+            <span class="btn-inner--icon">
+                <i class="fas fa-plus"></i>
+            </span>
+            <span class="btn-inner--text">Incluir / Salvar Alteração</span>
+        </button> 
+        `
+    },
+
+    changeUpdate: function (codigo, row) {
+        arrays.arrayChangeRows.push(row)
+        arrays.arrayChangeCods.push(codigo)
+
+        document.getElementById("changeButtons").innerHTML = `
+        <button type="button" class="btn btn-primary btn-icon-label" id="btn-atualizar" onclick="update(arrays.arrayChangeCods[${arrays.contadorChangeCods++}], arrays.arrayChangeRows[${arrays.contadorChangeRows++}])">
+            <span class="btn-inner--icon">
+                <i class="fas fa-plus"></i>
+            </span>
+            <span class="btn-inner--text">Salvar Alteração</span>
+        </button> 
         `
     }
 }
 
-function change(){
-    breakModal.buttonInsert();
-}
+changeButtonInsert.addEventListener('click', () => {
+    //Quebra de referência de modais
+    breakModal.changeInsert()
+})
 
-/*function insertFER(){
-    const dtCadastro = $('#dataCadastro').val()
-    const parceiro = $('#parceiro').val()
-    const cpf = $('#cpf').val()
+changeButtonUpdate.addEventListener('click', () => {
+    breakModal.changeUpdate("")
+})
+
+buttonSearch.addEventListener('click', () => {
+    var node = document.getElementById("List");
+    while (node.hasChildNodes()) {
+        node.removeChild(node.lastChild);
+    }
 
     const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json")
+    myHeaders.append("Content-Type", "application/json");
+
+    const usuario = $('#usuario-consulta').val()
+    const status = $('#status-consulta').val()
+    const dtAtualizacao = $('#dataAtualização-consulta').val()
+    const dtCadastro = $('#dataCadastro-consulta').val()
+    const cpf = $('#cpf-consulta').val()
 
     const body = {
-        data_cadastro: dtCadastro,
-        parceiro:parceiro,
-        cpf:cpf
+        usuario: usuario, //não sei se esta certo
+        status: status,
+        data_atualizacao: dtAtualizacao,
+        data_envio: dtCadastro,
+        cpf: cpf
     }
 
     const raw = JSON.stringify(body)
 
-    let requestOptions = {
+    const requestOptions = {
         method: 'POST',
         headers: myHeaders,
         body: raw,
         redirect: 'follow'
     }
 
-    fetch(URL + "", requestOptions).
-    then(response => response.text()).
-    then(function (res) {
+    fetch(URL + '/user/saldofer/filtro', requestOptions).
+    then(response => response.json().then(function (data) {
+        arrays.arrayCods = []
 
-    }).catch(error => console.log('erro: ', error))
-}*/
+        for (let i = 0; i < data.length; i++) {
+            let specific_tbody = document.getElementById('List');
+            let row = specific_tbody.insertRow(-1);
+            let cpf = row.insertCell(-1);
+            let dtCadastro = row.insertCell(-1);
+            let status = row.insertCell(-1);
+            let parceiro = row.insertCell(-1);
+            let dtAtualizacao = row.insertCell(-1);
+            let log = row.insertCell(-1);
+            let alteraThis = row.insertCell(-1);
+
+            let cpfText = document.createTextNode(`${data[i].cpf}`);
+            cpf.appendChild(cpfText);
+
+            let dtCadastroText = document.createTextNode(`${data[i].data_envio}`);
+            dtCadastro.appendChild(dtCadastroText);
+
+            let statusText = document.createTextNode(`${data[i].status}`);
+            status.appendChild(statusText);
+
+            let parceiroText = document.createTextNode(`${data[i].parceiro}`);
+            parceiro.appendChild(parceiroText);
+
+            let dataAtualizacaoText = document.createTextNode(`${data[i].data_atualizacao}`);
+            dtAtualizacao.appendChild(dataAtualizacaoText);
+
+            let logText = document.createTextNode(``);
+            log.appendChild(logText);
+
+            arrays.arrayCods.push(data[i].codigo);
+            arrays.arrayRows.push(row)
+
+            alteraThis.innerHTML = `<div class="actions ml-3" style="text-align: center;">
+            <a href="#" class="action-item mr-2" data-toggle="modal"
+                data-target=".modal-alterar-consultasaldofer" title="Alterar" id="icon-alterar" onclick="updateFields(arrays.arrayCods[${i}], arrays.arrayRows[${i}])">
+                <i class="fas fa-external-link-alt"></i>
+            </a>
+
+        </div>
+            `
+
+
+        }
+    }))
+
+})
+
+
+function insert() {
+    const data_cadastro = $("#dataCadastro").val()
+    const parceiro = $("#parceiro").val()
+    const cpf = $("#cpf").val()
+
+    const myheaders = new Headers();
+    myheaders.append('Content-Type', 'application/json')
+
+    const body = {
+        data_envio: data_cadastro,
+        parceiro: parceiro,
+        cpf: cpf
+    }
+
+    const raw = JSON.stringify(body);
+
+    const requestOptions = {
+        method: 'POST',
+        headers: myheaders,
+        body: raw,
+        redirect: 'follow'
+    }
+
+    fetch(URL + '/user/saldofer/incluir', requestOptions).
+    then(response => response.json().then(function (data) {
+
+        const resultInsert = insertAnexo(data)
+        Promise.resolve(resultInsert).then(function (value) {
+            // console.log(value); // "Success"
+            $('#messageSuccess').show();
+            $('#messageSuccess').fadeIn(300).delay(3000).fadeOut(400);
+            document.getElementById("messageSuccess").textContent = "Saldo FER incluido com sucesso"
+        })
+
+    })).catch(error => console.log('erro: ', error))
+}
+
+async function insertAnexo(data) {
+
+    const fileInputs = document.querySelectorAll('div#div-fundo input[type="file"]')[0]
+    const codigo = data.codigo
+
+    var data = new FormData()
+    data.append(fileInputs.name, fileInputs.files[0])
+
+    await fetch(URL + `/user/saldofer/incluir/arquivos?codigo=${codigo}`, {
+        method: 'POST',
+        body: data
+    }).
+    then(response => response.json().then(function (data) {
+        obj.transporter = data
+    })).catch(error => console.log('error: ', error))
+
+    return obj.transporter;
+}
+
+function updateFields(cod, row) {
+    breakModal.changeUpdate(cod, row)
+
+
+    const myheaders = new Headers()
+    myheaders.append('Content-Type', 'application/json')
+
+    const body = {
+        codigo: cod
+    }
+
+    const raw = JSON.stringify(body)
+
+    const requestOptions = {
+        method: 'POST',
+        headers: myheaders,
+        body: raw,
+        redirect: 'follow'
+    }
+
+    fetch(URL + '/user/saldofer/modal', requestOptions).
+    then(response => response.json().then(function (data) {
+        $("#dataCadastro").val(data.data_envio)
+        $("#parceiro").val(data.parceiro)
+        $("#cpf").val(data.cpf)
+
+        //OBS TRAZER O ARQUIVO1 E SETAR NO BUTTON DOWNLOAD
+
+    })).catch(error => console.log('error: ', error))
+
+
+}
+
+function update(codigo, row) {
+    const myheaders = new Headers()
+    myheaders.append('Content-Type', 'application/json')
+
+    const data_cadastro = $("#dataCadastro").val()
+    const parceiro = $("#parceiro").val()
+    const cpf = $("#cpf").val()
+
+    
+
+    const body = {
+        codigo: codigo,
+        data_envio: data_cadastro,
+        parceiro: parceiro,
+        cpf: cpf
+    }
+
+    const raw = JSON.stringify(body)
+
+    const requestOptions = {
+        method: 'POST',
+        headers: myheaders,
+        body: raw,
+        redirect: 'follow'
+    }
+
+    fetch(URL+'/user/saldofer/atualizar', requestOptions).
+    then(response => response.json().then(function (data){
+        if(data) {
+            $('#messageSuccess').show();
+            $('#messageSuccess').fadeIn(300).delay(3000).fadeOut(400);
+            document.getElementById("messageSuccess").textContent = "Saldo FER atualizado com sucesso"
+
+            updateTbody(row, data.status)
+        }
+        
+    })).catch(error => console.log('error: ', error))
+    
+}
+
+function updateTbody(row, status) {
+    let cells = row.cells
+    cells[0].textContent = $("#cpf").val()
+    cells[1].textContent = $("#dataCadastro").val()
+    cells[2].textContent = status
+    cells[3].textContent = $("#parceiro").val()
+
+}
