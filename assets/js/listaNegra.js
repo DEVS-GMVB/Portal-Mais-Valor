1b6ddf4ce14087cb567f4ccaeb0328fecda4e26d
KainanGV <71570785+KainanGV@users.noreply.github.com>
2021-03-16 12:24:44 -0300

    finalizado lista negra

--- assets/js/listaNegra.js
+++ assets/js/listaNegra.js
@@ -2,6 +2,12 @@ const URL = 'http://localhost:3000';
 
 const insert = document.getElementById("insert");
 
+
+const arrays = {
+    arraysReferenceRow: [],
+    contadorRow: 0
+}
+
 const functionsRequests = {
     insert: () => {
         const cpf_cliente = $("#cpf_cliente").val();
@@ -24,17 +30,19 @@ const functionsRequests = {
             redirect: 'follow'
         };
 
-        fetch(URL+"/user/lista/negra/inserir", requestOptions)
+        fetch(URL + "/user/lista/negra/inserir", requestOptions)
             .then(response => response.json())
             .then(result => {
 
-                console.log(result);
-                if(result.message === "Cpf já existente na lista negra") {
+                if (result.message === "Cpf já existente na lista negra") {
                     $('#error').show();
                     $('#error').fadeIn(300).delay(3000).fadeOut(400);
                     document.getElementById("error").textContent = "CPF cliente já existe na Lista Negra";
 
                 } else {
+
+                    insertTbody(result)
+
                     $('#success').show();
                     $('#success').fadeIn(300).delay(3000).fadeOut(400);
                     document.getElementById("success").textContent = "CPF cliente incluido com sucesso na Lista Negra";
@@ -43,6 +51,7 @@ const functionsRequests = {
             .catch(error => console.log('error', error));
     }
 }
+
 
 const handleFunctionsRequests = {
     get: (target, name) => {
@@ -67,5 +76,50 @@ const handleFunctionsRequests = {
 const functionsRequestsProxy = new Proxy(functionsRequests, handleFunctionsRequests)
 
 insert.addEventListener('click', () => {
-    functionsRequestsProxy.insert()
+    if ($("#cpf_cliente").val() == "") {
+        $('#error').show();
+        $('#error').fadeIn(300).delay(3000).fadeOut(400);
+        return document.getElementById("error").textContent = "Preencha o campo CPF cliente";
+    }
+    functionsRequestsProxy.insert();
 })
+
+function insertTbody(data) {
+
+    arrays.contadorRow++;
+
+    let listNegra = document.getElementById("listNegra");
+    let row = listNegra.insertRow(-1);
+    let cpf_cell = row.insertCell(-1);
+    let motivo_cell = row.insertCell(-1);
+    let usuario_cell = row.insertCell(-1);
+    let delete_icon = row.insertCell(-1);
+
+    let cpf_cell_text = document.createTextNode(`${data.cpf}`);
+    cpf_cell.appendChild(cpf_cell_text);
+
+    let motivo_cell_text = document.createTextNode(`${data.motivo}`)
+    motivo_cell.appendChild(motivo_cell_text);
+
+    let usuario_cell_text = document.createTextNode(`${data.parceiro}`)
+    usuario_cell.appendChild(usuario_cell_text);
+
+    console.log($('#listNegra tr').length)
+
+    delete_icon.innerHTML = `
+        <div class="actions ml-3">
+            <a href="#" class="action-item mr-2" data-toggle="tooltip" title="Excluir" onclick="deleteRow(arrays.contadorRow)">
+                <i class="fas fa-trash-alt"></i>
+            </a>
+        </div>
+    `
+}
+
+
+function deleteRow(row) {
+    
+    console.log(row);
+
+    document.getElementById("listNegra").deleteRow($('#listNegra tr')[row]);
+    arrays.contadorRow--;
+}
