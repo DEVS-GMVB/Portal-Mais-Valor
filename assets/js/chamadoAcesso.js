809195f0e8779a86b6247f58a299554863ac58a9
Cauesilvaa <43147371+Cauesilvaa@users.noreply.github.com>
2021-07-13 16:52:17 -0300

    acesso chamado finalizado

--- assets/js/chamadoAcesso.js
+++ assets/js/chamadoAcesso.js
@@ -3,11 +3,13 @@ const URL = `http://localhost:3000/user`;
 
 //Map
 const mapObjs = new Map();
+const mapHash = new Map();
 
 //Btn's
 const incluir = document.getElementById('btn-incluir');
 const iconUpdate = document.getElementById('icon-update');
 const filtroBtn = document.getElementById("btnBuscar");
+const alterarBtn = document.getElementById("btn-alterar");
 
 //Check Box's
 let bra = document.getElementById('check-bradesco');
@@ -121,6 +123,7 @@ filtroBtn.addEventListener('click', (e) => {
     fetch(`${URL}/acessochamado/filtro`, requestOptions)
         .then(response => response.json().then(data => {
             for (let i in data) {
+                console.log(data);
                 mapObjs.set(data[i].id, data[i]);
 
                 let specific_tbody = document.getElementById('listItens');
@@ -186,17 +189,115 @@ function preencherModal(data) {
     $("#campo-nome2").val(data.nome);
     $("#campo-cpf2").val(data.cpf);
     $("#campo-rg2").val(data.rg);
-    $("#campo-nascimento2").val(data.data_nasimento);
+    $("#campo-nascimento2").val(data.data_nascimento);
     $("#campo-mae-nome2").val(data.nome_mae);
     $("#campo-certificacao2").val(data.certificacao);
     $("#campo-telefone2").val(data.telefone_celular);
+    $("#campo-endereco2").val(data.endereco);
     $("#campo-complemento2").val(data.complemento);
     $("#campo-bairro2").val(data.bairro);
-    $("#campo-cidade2").val(data.campo-cidade2);
-    $("#campo-uf2").val(data.uf);
     $("#campo-cidade2").val(data.cidade);
-    $("#check-bradesco2").val(data.bradesco);
-    $("#check-crefisa2").val(data.crefisa);
+    $("#campo-uf2").val(data.uf);
+    $("#campo-cep2").val(data.cep);
+    $("#bradesco-login").val(data.bradesco_login);
+    $("#bradesco-senha").val(data.bradesco_senha);
+    $("#crefisa-login").val(data.crefisa_login);
+    $("#crefisa-senha").val(data.crefisa_senha);
+    $("#Pan-login").val(data.pan_login);
+    $("#Pan-senha").val(data.pan_senha);
+    $("#Sim-login").val(data.sim_login);
+    $("#Sim-senha").val(data.sim_senha);
+    $("#cetelem-login").val(data.cetelem_login);
+    $("#cetelem-senha").val(data.cetelem_senha);
+    $("#daycoval-login").val(data.daycoval_login);
+    $("#daycoval-senha").val(data.daycoval_senha);
+    $("#parana-login").val(data.parana_login);
+    $("#parana-senha").val(data.parana_senha);
+    $("#f5bmg-login").val(data.f5bmg_login);
+    $("#f5bmg-senha").val(data.f5bmg_senha);
+    $("#consorcio-bb-login").val(data.consorciobb_login);
+    $("#consorcio-bb-senha").val(data.consorciobb_senha);
+    $("#imobiliario-login").val(data.imobiliario_login);
+    $("#imobiliario-senha").val(data.imobiliario_senha);
+    $("#santander-login").val(data.santander_login);
+    $("#santander-senha").val(data.santander_senha);
+    $("#wimo-login").val(data.consorciowimo_login);
+    $("#wimo-senha").val(data.consorciowimo_senha);
+    $("#itau-login").val(data.itau_login);
+    $("#itau-senha").val(data.itau_senha);
+    $("#safra-login").val(data.safra_login);
+    $("#safra-senha").val(data.safra_senha);
+    $("#indica-login").val(data.bbindicavendas_login);
+    $("#indica-senha").val(data.bbindicavendas_senha);
+    $("#ole-login").val(data.ole_login);
+    $("#ole-senha").val(data.ole_senha);
+    $("#banco-brasil-login").val(data.bancobrasil_login);
+    $("#banco-brasil-senha").val(data.bancobrasil_senha);
+    $("#esteira-login").val(data.esteiraportal_login);
+    $("#esteira-senha").val(data.esteiraportal_senha);
+    $("#crm-login").val(data.crm_login);
+    $("#crm-senha").val(data.crm_senha);
+
+    mapHash.set(data.id, data);
+
+    verificaCampos(data);
+
+    alterarBtn.setAttribute('name', `${data.id}`);
+
+    document.getElementById('rg_arq2_file').innerHTML =
+        `<button type="button" class="btn btn-primary btn-icon-label" id="rg_arq2" name="rg_arq" 
+        onclick="downloadRg(mapHash.get(${data.id}))" style="background-color: white; color: gray; border-color: gray;">
+        <span class="btn-inner--icon">
+                                        <i class="fa fa-upload"></i>
+                                    </span>    
+        <span class="btn-inner--text">Rg :</span>
+        </button>`
+
+    document.getElementById('cpf_arq2_file').innerHTML =
+        `<button type="button" class="btn btn-primary btn-icon-label" id="cpf_arq2" name="cpf_arq2" 
+    onclick="downloadCpf(mapHash.get(${data.id}))" style="background-color: white; color: gray; border-color: gray;">
+    <span class="btn-inner--icon">
+                                        <i class="fa fa-upload"></i>
+                                    </span>     
+    <span class="btn-inner--text">Cpf :</span>
+    </button>`
+
+    document.getElementById('aneps_arq2_file').innerHTML =
+        `<button type="button" class="btn btn-primary btn-icon-label" id="aneps_arq" name="aneps_arq" 
+    onclick="downloadAneps(mapHash.get(${data.id}))" style="background-color: white; color: gray; border-color: gray;">
+    <span class="btn-inner--icon">
+                                        <i class="fa fa-upload"></i>
+                                    </span>     
+    <span class="btn-inner--text">Aneps :</span>
+    </button>`
+
+
+    document.getElementById('pis_arq2_file').innerHTML =
+        `<button type="button" class="btn btn-primary btn-icon-label" id="pis_arq" name="pis_arq" 
+    onclick="downloadPis(mapHash.get(${data.id}))" style="background-color: white; color: gray; border-color: gray;">
+    <span class="btn-inner--icon">
+                                        <i class="fa fa-upload"></i>
+                                    </span> 
+                                    <span class="btn-inner--text">Pis :</span>
+    </button>`
+
+    document.getElementById('titulo_arq2_file').innerHTML =
+        `<button type="button" class="btn btn-primary btn-icon-label" id="titulo_arq" name="titulo_arq" 
+    onclick="downloadTitulo(mapHash.get(${data.id}))" style="background-color: white; color: gray; border-color: gray;">
+    <span class="btn-inner--icon">
+                                        <i class="fa fa-upload"></i>
+                                    </span>     
+    <span class="btn-inner--text">Titulo :</span>
+    </button>`
+
+    document.getElementById('comprovante_endereco_arq2_file').innerHTML =
+        `<button type="button" class="btn btn-primary btn-icon-label" id="comprovante_endereco_arq2" name="comprovante_endereco_arq2" 
+    onclick="downloadEnd(mapHash.get(${data.id}))" style="background-color: white; color: gray; border-color: gray;">
+    <span class="btn-inner--icon">
+                                        <i class="fa fa-upload"></i>
+                                    </span>     
+    <span class="btn-inner--text">Pis :</span>
+    </button>`
 }
 
 
@@ -286,22 +387,148 @@ incluir.addEventListener('click', () => {
 
     fetch(URL + "/acessochamado/incluir", requestOptions).
     then(response => response.json()).
-    then(function (res) {
-        console.log(body)
+    then(async function (res) {
+        //console.log(body)
         $('#alert-sucesso').show();
         $('#alert-sucesso').fadeIn(300).delay(3000).fadeOut(400);
         document.getElementById("alert-sucesso").textContent = "Chamado de acesso incluido"
+
+        const dataJson = await uploadFiles(res.id);
+        console.log(dataJson);
+        if (dataJson) {
+            console.log(dataJson);
+        }
     }).catch(error => console.log('erro: ', error));
 });
 
-iconUpdate.addEventListener('click', () => {
+alterarBtn.addEventListener('click', (e) => {
+    const id = alterarBtn.getAttribute('name');
+
+    const bradesco_login = $("#bradesco-login").val()
+    const bradesco_senha = $("#bradesco-senha").val()
+    const crefisa_login = $("#crefisa-login").val()
+    const crefisa_senha = $("#crefisa-senha").val()
+    const pan_login = $("#Pan-login").val()
+    const pan_senha = $("#Pan-senha").val()
+    const sim_login = $("#Sim-login").val()
+    const sim_senha = $("#Sim-senha").val()
+    const cetelem_login = $("#cetelem-login").val()
+    const cetelem_senha = $("#cetelem-senha").val()
+    const daycoval_login = $("#daycoval-login").val()
+    const daycoval_senha = $("#daycoval-senha").val()
+    const parana_login = $("#parana-login").val()
+    const parana_senha = $("#parana-senha").val()
+    const f5bmg_login = $("#f5bmg-login").val()
+    const f5bmg_senha = $("#f5bmg-senha").val()
+    const consorciobb_login = $("#consorcio-bb-login").val()
+    const consorciobb_senha = $("#consorcio-bb-senha").val()
+    const imobiliario_login = $("#imobiliario-login").val()
+    const imobiliario_senha = $("#imobiliario-senha").val()
+    const santander_login = $("#santander-login").val()
+    const santander_senha = $("#santander-senha").val()
+    const consorciowimo_login = $("#wimo-login").val()
+    const consorciowimo_senha = $("#wimo-senha").val()
+    const itau_login = $("#itau-login").val()
+    const itau_senha = $("#itau-senha").val()
+    const safra_login = $("#safra-login").val()
+    const safra_senha = $("#safra-senha").val()
+    const bbindicavendas_login = $("#indica-login").val()
+    const bbindicavendas_senha = $("#indica-senha").val()
+    const ole_login = $("#ole-login").val()
+    const ole_senha = $("#ole-senha").val()
+    const bancobrasil_login = $("#banco-brasil-login").val()
+    const bancobrasil_senha = $("#banco-brasil-senha").val()
+    const esteiraportal_login = $("#esteira-login").val()
+    const esteiraportal_senha = $("#esteira-senha").val()
+    const crm_login = $("#crm-login").val()
+    const crm_senha = $("#crm-senha").val()
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        bradesco_login,
+        bradesco_senha,
+        crefisa_login,
+        crefisa_senha,
+        pan_login,
+        pan_senha,
+        sim_login,
+        sim_senha,
+        cetelem_login,
+        cetelem_senha,
+        daycoval_login,
+        daycoval_senha,
+        parana_login,
+        parana_senha,
+        f5bmg_login,
+        f5bmg_senha,
+        consorciobb_login,
+        consorciobb_senha,
+        imobiliario_login,
+        imobiliario_senha,
+        santander_login,
+        santander_senha,
+        consorciowimo_login,
+        consorciowimo_senha,
+        itau_login,
+        itau_senha,
+        safra_login,
+        safra_senha,
+        bbindicavendas_login,
+        bbindicavendas_senha,
+        ole_login,
+        ole_senha,
+        bancobrasil_login,
+        bancobrasil_senha,
+        esteiraportal_login,
+        esteiraportal_senha,
+        crm_login,
+        crm_senha,
+        data_alteracao: dateNow(),
+        responsavel: dataSession.nome
+    });
+
+    var requestOptions = {
+        method: 'PUT',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch(`${URL}/acessochamado/atualizar?id=${id}`, requestOptions)
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+
+});
+
+async function uploadFiles(id) {
+    const data = new FormData();
+    const filesInputs = document.querySelectorAll("#files input[type='file']")
+
+    filesInputs.forEach(file => {
+        data.append(file.name, file.files[0]);
+    });
+
+    const dataResponse = await fetch(`${URL}/acessochamado/arquivo?id=${id}`, {
+        method: "POST",
+        body: data
+    }).then(response => response.json()).catch(error => console.log(error));
+
+    //console.log(dataResponse);
+
+    return dataResponse;
+}
+
+function verificaCampos(data) {
     const camposText = document.querySelectorAll("#dados-alteracao input[type='text']");
 
     camposText.forEach(element => {
         element.setAttribute('disabled', true);
     });
 
-    if (bra.checked) {
+    if (data.bradesco === '1') {
         $("#bradesco-login").attr('disabled', false);
         $("#bradesco-senha").attr('disabled', false);
     } else {
@@ -309,7 +536,7 @@ iconUpdate.addEventListener('click', () => {
         $("#bradesco-senha").attr('disabled', true);
     }
 
-    if (crefisa.checked) {
+    if (data.crefisa === '1') {
         $("#crefisa-login").attr('disabled', false);
         $("#crefisa-senha").attr('disabled', false);
     } else {
@@ -317,7 +544,7 @@ iconUpdate.addEventListener('click', () => {
         $("#crefisa-senha").attr('disabled', true);
     }
 
-    if (pan.checked) {
+    if (data.pan === '1') {
         $("#Pan-login").attr('disabled', false);
         $("#Pan-senha").attr('disabled', false);
     } else {
@@ -325,7 +552,7 @@ iconUpdate.addEventListener('click', () => {
         $("#Pan-senha").attr('disabled', true);
     }
 
-    if (sim.checked) {
+    if (data.sim === '1') {
         $("#Sim-login").attr('disabled', false);
         $("#Sim-senha").attr('disabled', false);
     } else {
@@ -333,7 +560,7 @@ iconUpdate.addEventListener('click', () => {
         $("#Sim-senha").attr('disabled', true);
     }
 
-    if (cetelem.checked) {
+    if (data.cetelem === '1') {
         $("#cetelem-login").attr('disabled', false);
         $("#cetelem-senha").attr('disabled', false);
     } else {
@@ -341,7 +568,7 @@ iconUpdate.addEventListener('click', () => {
         $("#cetelem-senha").attr('disabled', true);
     }
 
-    if (daycoval.checked) {
+    if (data.daycoval === '1') {
         $("#daycoval-login").attr('disabled', false);
         $("#daycoval-senha").attr('disabled', false);
     } else {
@@ -349,7 +576,7 @@ iconUpdate.addEventListener('click', () => {
         $("#daycoval-senha").attr('disabled', true);
     }
 
-    if (parana.checked) {
+    if (data.parana === '1') {
         $("#parana-login").attr('disabled', false);
         $("#parana-senha").attr('disabled', false);
     } else {
@@ -357,7 +584,7 @@ iconUpdate.addEventListener('click', () => {
         $("#parana-senha").attr('disabled', true);
     }
 
-    if (f5Bmg.checked) {
+    if (data.f5bmg === '1') {
         $("#f5bmg-login").attr('disabled', false);
         $("#f5bmg-senha").attr('disabled', false);
     } else {
@@ -365,7 +592,7 @@ iconUpdate.addEventListener('click', () => {
         $("#f5bmg-senha").attr('disabled', true);
     }
 
-    if (consorcioBB.checked) {
+    if (data.consorciobb === '1') {
         $("#consorcio-bb-login").attr('disabled', false);
         $("#consorcio-bb-senha").attr('disabled', false);
     } else {
@@ -373,7 +600,7 @@ iconUpdate.addEventListener('click', () => {
         $("#consorcio-bb-senha").attr('disabled', true);
     }
 
-    if (imobiliario.checked) {
+    if (data.imobiliario === '1') {
         $("#imobiliario-login").attr('disabled', false);
         $("#imobiliario-senha").attr('disabled', false);
     } else {
@@ -381,7 +608,7 @@ iconUpdate.addEventListener('click', () => {
         $("#imobiliario-senha").attr('disabled', true);
     }
 
-    if (santander.checked) {
+    if (data.santander === '1') {
         $("#santander-login").attr('disabled', false);
         $("#santander-senha").attr('disabled', false);
     } else {
@@ -389,7 +616,7 @@ iconUpdate.addEventListener('click', () => {
         $("#santander-senha").attr('disabled', true);
     }
 
-    if (wimo.checked) {
+    if (data.consorciowimo === '1') {
         $("#wimo-login").attr('disabled', false);
         $("#wimo-senha").attr('disabled', false);
     } else {
@@ -397,7 +624,7 @@ iconUpdate.addEventListener('click', () => {
         $("#wimo-senha").attr('disabled', true);
     }
 
-    if (itau.checked) {
+    if (data.itau === '1') {
         $("#itau-login").attr('disabled', false);
         $("#itau-senha").attr('disabled', false);
     } else {
@@ -405,7 +632,7 @@ iconUpdate.addEventListener('click', () => {
         $("#itau-senha").attr('disabled', true);
     }
 
-    if (safra.checked) {
+    if (data.safra === '1') {
         $("#safra-login").attr('disabled', false);
         $("#safra-senha").attr('disabled', false);
     } else {
@@ -413,7 +640,7 @@ iconUpdate.addEventListener('click', () => {
         $("#safra-senha").attr('disabled', true);
     }
 
-    if (vendas.checked) {
+    if (data.bbindicavendas === '1') {
         $("#indica-login").attr('disabled', false);
         $("#indica-senha").attr('disabled', false);
     } else {
@@ -421,7 +648,7 @@ iconUpdate.addEventListener('click', () => {
         $("#indica-senha").attr('disabled', true);
     }
 
-    if (ole.checked) {
+    if (data.ole === '1') {
         $("#ole-login").attr('disabled', false);
         $("#ole-senha").attr('disabled', false);
     } else {
@@ -429,7 +656,7 @@ iconUpdate.addEventListener('click', () => {
         $("#ole-senha").attr('disabled', true);
     }
 
-    if (bancoBrasil.checked) {
+    if (data.bancobrasil === '1') {
         $("#banco-brasil-login").attr('disabled', false);
         $("#banco-brasil-senha").attr('disabled', false);
     } else {
@@ -437,7 +664,7 @@ iconUpdate.addEventListener('click', () => {
         $("#banco-brasil-senha").attr('disabled', true);
     }
 
-    if (esteira.checked) {
+    if (data.esteiraportal === '1') {
         $("#esteira-login").attr('disabled', false);
         $("#esteira-senha").attr('disabled', false);
     } else {
@@ -445,11 +672,66 @@ iconUpdate.addEventListener('click', () => {
         $("#esteira-senha").attr('disabled', true);
     }
 
-    if (crm.checked) {
+    if (data.crm === '1') {
         $("#crm-login").attr('disabled', false);
         $("#crm-senha").attr('disabled', false);
     } else {
         $("#crm-login").attr('disabled', true);
         $("#crm-senha").attr('disabled', true);
     }
-})
+}
+
+
+function downloadRg(rg) {
+    console.log(rg.rg_arq);
+    if (rg.rg_arq) {
+        window.location.href = `http://localhost:3000/user/acessochamado/download?hash=${rg.rg_arq}`;
+    } else {
+        alert("Arquivo não inserido na base de dados");
+    }
+}
+
+function downloadCpf(cpf) {
+    console.log(cpf.cpf_arq);
+    if (cpf.cpf_arq) {
+        window.location.href = `http://localhost:3000/user/acessochamado/download?hash=${cpf.cpf_arq}`;
+    } else {
+        alert("Arquivo não inserido na base de dados");
+    }
+}
+
+function downloadAneps(aneps) {
+    console.log(aneps.aneps_arq);
+    if (aneps.aneps_arq) {
+        window.location.href = `http://localhost:3000/user/acessochamado/download?hash=${aneps.aneps_arq}`;
+    } else {
+        alert("Arquivo não inserido na base de dados");
+    }
+}
+
+function downloadPis(pis) {
+    console.log(pis.pis_arq);
+    if (pis.pis_arq) {
+        window.location.href = `http://localhost:3000/user/acessochamado/download?hash=${pis.pis_arq}`;
+    } else {
+        alert("Arquivo não inserido na base de dados");
+    }
+}
+
+function downloadTitulo(titulo) {
+    console.log(titulo.titulo_arq);
+    if (titulo.titulo_arq) {
+        window.location.href = `http://localhost:3000/user/acessochamado/download?hash=${titulo.titulo_arq}`;
+    } else {
+        alert("Arquivo não inserido na base de dados");
+    }
+}
+
+function downloadEnd(end) {
+    console.log(end.comprovante_endereco_arq);
+    if (end.comprovante_endereco_arq) {
+        window.location.href = `http://localhost:3000/user/acessochamado/download?hash=${end.comprovante_endereco_arq}`;
+    } else {
+        alert("Arquivo não inserido na base de dados");
+    }
+}
